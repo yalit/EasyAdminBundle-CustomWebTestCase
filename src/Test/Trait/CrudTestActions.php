b6132fa2aae934983492c0c1938129b03595c9ae
Yannick Alsberge <y.alsberge@yalit.be>
2023-02-12 15:26:52 +0100

    feat : add clickOnGlobalAction

--- src/Test/Trait/CrudTestActions.php
+++ src/Test/Trait/CrudTestActions.php
@@ -4,18 +4,29 @@ declare(strict_types=1);
 
 namespace EasyCorp\Bundle\EasyAdminBundle\Test\Trait;
 
-//TODO : implement TODOs ;-)
+use function PHPUnit\Framework\assertCount;
+
 trait CrudTestActions
 {
     protected function clickOnIndexEntityAction(string|int $entityId, string $action): void
     {
-        // TODO : to implement
+        $crawler = $this->client->getCrawler();
+    }
+
+    protected function clickOnIndexGlobalAction(string $globalAction): void
+    {
+        $crawler = $this->client->getCrawler();
+        $action = $crawler->filter(sprintf('.global-actions .action-%s', $globalAction));
+
+        assertCount(1, $action, sprintf('There is no action %s in the page', $globalAction));
+
+        $this->client->click($action->link());
     }
 
     /**
      * @param array<array-key, string> $entityIds
      */
-    protected function clickOnIndexGlobalAction(string $globalAction, array $entityIds = []): void
+    protected function clickOnIndexBatchAction(string $batchAction, array $entityIds = []): void
     {
         // TODO : to implement
     }
